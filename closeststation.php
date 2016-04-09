b1a3e689250b87cba6dd9288547ad4274d69149e
desca <david.klausz@gmail.com>
2016-04-09 17:36:22 +0200

    Closest station php service

---
+++ closeststation.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+require 'SplClassLoader.php';
+
+$classLoader = new SplClassLoader();
+$classLoader->register();
+
+if (
+    empty($_REQUEST['currentPositionLat'])
+    || empty($_REQUEST['currentPositionLng'])
+) {
+    exit(json_encode(['status' => 'ERROR', 'error_code' => 'MISSING_PARAMETER']));
+}
+
+$stationFinder = new StationFinder();
+
+$closestStationToDeparture = $stationFinder->closest(
+    $stationFinder->whereThereAreAvailableBikes(),
+    $_REQUEST['currentPositionLat'],
+    $_REQUEST['currentPositionLng']
+);
+
+echo json_encode([
+    'status' => 'OK',
+    'stations' => $closestStationToDeparture,
+]);
